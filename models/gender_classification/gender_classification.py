ce3f43adcc6d9ef4c38f6bc966844fbb60452ba8
Dmitri Glusco <dima2492@gmail.com>
2019-05-14 19:03:28 +0300

    Added Skater VGG analysis

--- models/gender_classification/gender_classification.py
+++ models/gender_classification/gender_classification.py
@@ -7,6 +7,7 @@ from keras.models import load_model
 class GenderClassifier():
     MODEL_PATH = "gender_classification.model"
     CLASSES = ["man", "woman"]
+    IMAGE_SIZES = (96,96)
 
     def __init__(
         self, model_path=None, classes=None
@@ -21,13 +22,14 @@ class GenderClassifier():
         with open(path) as f:
             pass
 
-    def _load_image_by_path(self, image_path: str):
+    def load_image_by_path(self, image_path: str):
         self._check_if_file_exists(image_path)
         image = cv2.imread(image_path)
         return image
 
-    def _preprocess_image(self, image):
-        image = cv2.resize(image, (96,96))
+    def preprocess_image(self, image_path: str):
+        image = self.load_image_by_path(image_path)
+        image = cv2.resize(image, GenderClassifier.IMAGE_SIZES)
         image = image.astype("float") / 255.0
         image = img_to_array(image)
         image = np.expand_dims(image, axis=0)
@@ -39,11 +41,14 @@ class GenderClassifier():
         label = self._classes[idx]
         return label
 
-    def predict_by_image_path(self, image_path: str) -> str:
-        image = self._load_image_by_path(image_path)
-        image = self._preprocess_image(image)
+    def predict_by_image(self, image) -> str:
         prediction = self._model.predict(image)
         label = self._get_label_from_prediction(prediction)
+        return label
+
+    def predict_by_image_path(self, image_path: str) -> str:
+        image = self.preprocess_image(image_path)
+        label = self.predict_by_image(image)
         return label
 
     def get_model(self):
